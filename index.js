602c5ce18b0d9051e6e43f17a8581d5a317793b3
Sam <samevwro@gmail.com>
2025-05-22 20:41:21 -0600

    laptop Push

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 
 const tableContainer = document.getElementById("tableInput")
 var dbJsonSize = 0
-
-async function onLoadAddTableData() {
+dbIdLength()
+async function dbIdLength() {
+    await fetch("http://localhost:3000/Events")
+    .then(function(list){
+        return list.json()
+    })
+    .then(function(items){
+        let lastObjectIndex = items[(Object.keys(items).length)-1].id
+        let lastId = parseInt(lastObjectIndex)+1
+        console.log(lastId)
+        dbJsonSize =+lastId
+    })
+}
+async function onLoadAddTableData(list) {
     const URL = "http://localhost:3000/Events"
 
     fetch(URL)
@@ -10,30 +22,35 @@ async function onLoadAddTableData() {
         return response.json()
     })
     .then(function(message){
+        console.log(dbJsonSize)
         tableContainer.innerHTML = ""
         for(let i = 0; i < Object.keys(message).length; i++){
             const newRow = document.createElement("tr")
-            newRow.id = `row${i}`
+            newRow.id = `row${message[i].id}`
             newRow.className = "text-bg-secondary p-3"
             tableContainer.appendChild(newRow)
-            const appendedRow = document.getElementById(`row${i}`)
-            dbJsonSize =+ 1
+            const appendedRow = document.getElementById(`row${message[i].id}`)
             let eventHTML = `<td>${message[i].Event}</td>
                 <td>${message[i].StartDate}</td>
                 <td>${message[i].EndDate}</td>
                 <td>${message[i].Notes}</td>
                 <td><button onclick="deleteEventBtn(${i})" type="button" class="btn btn-danger" id="deleteBtn${i}">Delete</button></td>`
             appendedRow.insertAdjacentHTML('beforeend', eventHTML)
-        }
-    })
+            dbJsonSize =+ 1
+        }}
+    )
+    
 }
+
 onLoadAddTableData()
+
 
 async function onCreateEventClick(){
     const eventValue = document.getElementById('eventInput').value
     const notesValue = document.getElementById("notesInput").value
     const startDateValue = document.getElementById("startDate").value
     const endDateValue = document.getElementById("endDate").value
+    
     const JsonValues = { id: ""+dbJsonSize, Event: ""+eventValue, StartDate: ""+startDateValue, EndDate: ""+endDateValue, Notes: ""+notesValue }
     dbJsonSize =+ 1
     const response = await fetch("http://localhost:3000/Events", {
@@ -41,7 +58,10 @@ async function onCreateEventClick(){
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(JsonValues)
     })
-    eventValue.innerHTML = ""
+    document.getElementById('eventInput').value = ''
+    document.getElementById("notesInput").value = ''
+    document.getElementById("startDate").value = ''
+    document.getElementById("endDate").value = ''
     onLoadAddTableData()
     const newlyCreatedEvent = await response.json()
 }
@@ -54,3 +74,4 @@ async function deleteEventBtn(id){
     })
     onLoadAddTableData()
 }
+
